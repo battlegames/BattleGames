091cab56d4f90f4afb9096580d885d5209a5428b
anhcraft <admin@anhcraft.dev>
2019-08-21 12:27:04 +0700

    Support info placeholders for arena chooser

--- spigot/src/main/java/dev/anhcraft/abm/gui/ArenaChooserHandler.java
+++ spigot/src/main/java/dev/anhcraft/abm/gui/ArenaChooserHandler.java
@@ -1,18 +1,40 @@
 package dev.anhcraft.abm.gui;
 
 import com.google.common.collect.Multimap;
+import dev.anhcraft.abif.PreparedItem;
 import dev.anhcraft.abm.api.APIProvider;
 import dev.anhcraft.abm.api.BattleAPI;
+import dev.anhcraft.abm.api.game.Game;
 import dev.anhcraft.abm.api.gui.*;
+import dev.anhcraft.abm.api.misc.info.InfoHolder;
+import dev.anhcraft.abm.utils.ListUtil;
+import dev.anhcraft.abm.utils.PlaceholderUtils;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Map;
+import java.util.Optional;
 
 public class ArenaChooserHandler extends GuiHandler implements PaginationHandler {
     @Override
     public void pullData(Pagination pagination, Player player, Multimap<ItemStack, GuiListener<? extends SlotReport>> data) {
         BattleAPI api = APIProvider.get();
         api.listArenas().forEach(arena -> {
-            data.put(arena.getIcon().build(), new GuiListener<SlotClickReport>(SlotClickReport.class) {
+            InfoHolder infoHolder;
+            Optional<Game> gameOpt = APIProvider.get().getGameManager().getGame(arena);
+            if(gameOpt.isPresent()){
+                infoHolder = new InfoHolder("game_");
+                gameOpt.get().inform(infoHolder);
+            } else {
+                infoHolder = new InfoHolder("arena_");
+                arena.inform(infoHolder);
+            }
+            Map<String, String> infoMap = APIProvider.get().mapInfo(infoHolder);
+            PreparedItem icon = arena.getIcon();
+            icon.name(ChatColor.translateAlternateColorCodes('&', PlaceholderUtils.formatInfo(icon.name(), infoMap)));
+            ListUtil.update(icon.lore(), s -> ChatColor.translateAlternateColorCodes('&', PlaceholderUtils.formatInfo(s, infoMap)));
+            data.put(icon.build(), new GuiListener<SlotClickReport>(SlotClickReport.class) {
                 @Override
                 public void call(SlotClickReport event) {
                     event.getPlayer().closeInventory();
