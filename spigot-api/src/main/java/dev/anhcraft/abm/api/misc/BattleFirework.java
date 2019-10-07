f79d3479e9afbdf080debfab63f7f75c68010661
anhcraft <admin@anhcraft.dev>
2019-10-07 21:35:09 +0700

    Fixed firework did not use defined settings

--- spigot-api/src/main/java/dev/anhcraft/abm/api/misc/BattleFirework.java
+++ spigot-api/src/main/java/dev/anhcraft/abm/api/misc/BattleFirework.java
@@ -20,21 +20,27 @@
 
 package dev.anhcraft.abm.api.misc;
 
+import dev.anhcraft.confighelper.ConfigHelper;
 import dev.anhcraft.confighelper.ConfigSchema;
 import dev.anhcraft.confighelper.annotation.Explanation;
 import dev.anhcraft.confighelper.annotation.IgnoreValue;
 import dev.anhcraft.confighelper.annotation.Key;
 import dev.anhcraft.confighelper.annotation.Schema;
+import dev.anhcraft.confighelper.exception.InvalidValueException;
+import dev.anhcraft.confighelper.impl.TwoWayMiddleware;
 import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Firework;
 import org.bukkit.inventory.meta.FireworkMeta;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Schema
-public class BattleFirework {
+public class BattleFirework implements TwoWayMiddleware {
     public static ConfigSchema<BattleFirework> SCHEMA = ConfigSchema.of(BattleFirework.class);
 
     @Key("effects")
@@ -59,6 +65,41 @@ public class BattleFirework {
         Firework fw = location.getWorld().spawn(location, Firework.class);
         FireworkMeta fwm = fw.getFireworkMeta();
         fwm.setPower(power);
-        effects.forEach(e -> fwm.addEffect(e.getFireworkEffect()));
+        for (BattleFireworkEffect bfe : effects){
+            fwm.addEffect(bfe.getFireworkEffect());
+        }
+        fw.setFireworkMeta(fwm);
+    }
+
+    @Override
+    public @Nullable Object conf2schema(ConfigSchema.Entry entry, @Nullable Object value) {
+        if(value != null && entry.getKey().equals("effects")){
+            ConfigurationSection cs = (ConfigurationSection) value;
+            List<BattleFireworkEffect> list = new ArrayList<>();
+            for(String s : cs.getKeys(false)){
+                try {
+                    list.add(ConfigHelper.readConfig(cs.getConfigurationSection(s), BattleFireworkEffect.SCHEMA));
+                } catch (InvalidValueException e) {
+                    e.printStackTrace();
+                }
+            }
+            return list;
+        }
+        return value;
+    }
+
+    @Override
+    public @Nullable Object schema2conf(ConfigSchema.Entry entry, @Nullable Object value) {
+        if(value != null && entry.getKey().equals("effects")){
+            ConfigurationSection parent = new YamlConfiguration();
+            int i = 0;
+            for(BattleFireworkEffect b : (List<BattleFireworkEffect>) value){
+                YamlConfiguration c = new YamlConfiguration();
+                ConfigHelper.writeConfig(c, BattleFireworkEffect.SCHEMA, b);
+                parent.set(String.valueOf(i++), c);
+            }
+            return parent;
+        }
+        return value;
     }
 }
