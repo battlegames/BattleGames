427601599de937ad8eb4bca0cabfb7ed0cf0f180
anhcraft <admin@anhcraft.dev>
2019-11-16 08:24:06 +0700

    Prevents memory leak in simple team

--- spigot-api/src/main/java/dev/anhcraft/battle/api/game/SimpleTeam.java
+++ spigot-api/src/main/java/dev/anhcraft/battle/api/game/SimpleTeam.java
@@ -28,7 +28,7 @@ import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 
 public class SimpleTeam<T extends Team> implements Resettable {
-    private final Map<Player, T> PLAYER_MAP = new HashMap<>();
+    private final Map<Player, T> PLAYER_MAP = new WeakHashMap<>();
     private final Map<T, Integer> PLAYER_COUNTER = new HashMap<>();
     private final Object LOCK = new Object();
 
