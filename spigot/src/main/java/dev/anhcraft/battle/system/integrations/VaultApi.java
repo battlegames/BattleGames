88a7f46491c7ca52c2eb4d493f0ba2f1ee2980a2
anhcraft <admin@anhcraft.dev>
2019-11-18 00:10:01 +0700

    Updated VaultApi

--- spigot/src/main/java/dev/anhcraft/battle/system/integrations/VaultApi.java
+++ spigot/src/main/java/dev/anhcraft/battle/system/integrations/VaultApi.java
@@ -21,7 +21,6 @@ package dev.anhcraft.battle.system.integrations;
 
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.ServicesManager;
 
@@ -41,18 +40,5 @@ public class VaultApi {
 
     public static Economy getEconomyApi(){
         return eco;
-    }
-
-    public static boolean hasEnoughBalance(OfflinePlayer player, double balance){
-        return eco.getBalance(player) >= balance;
-    }
-
-    public static boolean setBalance(OfflinePlayer player, double newBalance){
-        if(eco == null) return false;
-        double delta = eco.getBalance(player)-newBalance;
-        double abs = Math.abs(delta);
-        if(delta == 0) return true;
-        else if(delta > 0) return eco.withdrawPlayer(player, abs).transactionSuccess();
-        else return eco.depositPlayer(player, abs).transactionSuccess();
     }
 }
