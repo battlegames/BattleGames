7aedbc60940ff51436857a9e7e8cf786c93b1beb
anhcraft <admin@anhcraft.dev>
2019-12-15 22:40:41 +0700

    Fixed parsed location had no yaw values

--- spigot-api/src/main/java/dev/anhcraft/battle/utils/LocationUtil.java
+++ spigot-api/src/main/java/dev/anhcraft/battle/utils/LocationUtil.java
@@ -39,16 +39,17 @@ public class LocationUtil {
 
     @NotNull
     public static Location fromString(@Nullable String str) {
-        if(str == null || (str = str.replaceAll("[^a-zA-Z0-9-. ~]", ""))
-                .equalsIgnoreCase("null")) return Bukkit.getWorlds().get(0).getSpawnLocation();
+        if(str == null || (str = str.replaceAll("[^a-zA-Z0-9-. ~]", "")).equalsIgnoreCase("null")) {
+            return Bukkit.getWorlds().get(0).getSpawnLocation();
+        }
         String[] str2loc = str.split(" ");
         Location loc = new Location(
                 str2loc[0].equals("~") ? null : Bukkit.getWorld(str2loc[0]),
                 Double.parseDouble(str2loc[1]),
                 Double.parseDouble(str2loc[2]),
                 Double.parseDouble(str2loc[3]));
-        if(str2loc.length == 5) loc.setYaw(Float.parseFloat(str2loc[4]));
-        if(str2loc.length > 5) loc.setPitch(Float.parseFloat(str2loc[5]));
+        if(str2loc.length >= 5) loc.setYaw(Float.parseFloat(str2loc[4]));
+        if(str2loc.length >= 6) loc.setPitch(Float.parseFloat(str2loc[5]));
         return loc;
     }
 }
