b32b5e2a5f095ad40f3069635fecade47476b80b
anhcraft <huynhduyanh123123@gmail.com>
2020-01-22 21:02:12 +0700

    Optimized output market.yml

--- spigot/src/main/java/dev/anhcraft/battle/cmd/EditorCommand.java
+++ spigot/src/main/java/dev/anhcraft/battle/cmd/EditorCommand.java
@@ -55,7 +55,7 @@ public class EditorCommand extends BaseCommand{
     @Subcommand("market save")
     @CommandPermission("battle.editor.market.save")
     public void marketSave(CommandSender sender){
-        ConfigHelper.writeConfig(plugin.getMarketConf(), Market.SCHEMA, plugin.getMarket());
+        ConfigHelper.writeConfig(plugin.getMarketConf(), Market.SCHEMA, plugin.getMarket(), ConfigHelper.newOptions().ignoreEmptyArray().ignoreEmptyList().ignoreFalse().ignoreZero());
         File f = new File(plugin.getEditorFolder(), "market."+System.currentTimeMillis()+".yml");
         try {
             plugin.getMarketConf().save(f);
