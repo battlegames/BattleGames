7add71651e324ccd7e6449aeaba38ddf51d69d32
anhcraft <huynhduyanh123123@gmail.com>
2020-05-03 11:31:20 +0700

    minor change

--- spigot-premium/src/main/java/dev/anhcraft/battle/premium/system/ArenaSettings.java
+++ spigot-premium/src/main/java/dev/anhcraft/battle/premium/system/ArenaSettings.java
@@ -32,6 +32,7 @@ import org.jetbrains.annotations.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("FieldMayBeFinal")
 @Schema
 public class ArenaSettings extends ConfigurableObject {
     @Key("empty_regions")
